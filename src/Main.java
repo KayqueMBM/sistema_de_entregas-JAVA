15a7e7d85714a18cebe5aaae05069ab45a98db24
KayqueMBM <kayque_moreira@esstudante.sesisenai.org.br>
2024-02-27 11:05:36 -0300

    sistema de entregas - JAVA.iml

--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,87 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        Usuario meusuario = new Usuario();
-        meusuario.Inserir();
-        meusuario.Excluir();
+        Scanner teclado = new Scanner(System.in);
 
-        Evento meuevento = new Evento();
-        meuevento.Inserir();
-        meuevento.Excluir();
+        System.out.println("Sistema de Eventos da Cidade Joinville");
+        int escolha = 0;
+        do{
+            System.out.println(" 1 - Cadastro de Usuários");
+            System.out.println(" 2 - Cadastro de Eventos");
+            System.out.println(" Digite sua opção: ");
+            escolha = teclado.nextInt();
+        }while (escolha != 1 && escolha != 2);
+
+        if (escolha == 1)
+        {
+            Usuarios meu_usuario = new Usuarios();
+            meu_usuario.Inserir();
+        }
+        if (escolha == 2)
+        {
+            Eventos meu_evento = new Eventos();
+            meu_evento.Menu();
+        }
     }
 }
 
 class Usuario{
-    public String Nome;
-    public String Endereco;
-    public String Senha;
-    void Inserir(){}
-    void Excluir(){}
 
-    public Usuario(String Nome, String Endereco, String Senha) {
-        this.Nome = nome;
-        this.Endereco = endereco;
-        this.Senha = senha;
+    public String Codigo;
+    public String Nome;
+    public String Fone;
+}
+class Usuarios{
+
+    Usuario s1 = new Usuario();
+    public void Inserir(){
+
+        s1.Codigo = "1";
+        s1.Nome = "Marcelo";
+        s1.Fone = "999156237";
+
+        Consultar();
     }
-    public String getNome() {
-        return nome;
-    }
-    public String getEndereco() {
-        return endereco;
-    }
-    public String getSenha() {
-        return senha;
-    }
-    @Override
-    public String toString() {
-        return "Nome: " + nome + ", Endereco: " + endereco ", Senha: " + senha;
+    public void Consultar(){
+        System.out.println(s1.Nome);
     }
 }
-class Evento{
+class Evento {
     public String Nome;
     public String Endereco;
     public String Categoria;
     public String Horario;
     public String Descricao;
+}
+
+class Eventos {
+    public void Menu(){
+
+        Scanner teclado = new Scanner(System.in);
+        int escolha = 0;
+        do{
+            System.out.println(" 1 - Inserir");
+            System.out.println(" 2 - Consultar");
+            System.out.println(" 3 - Consultar");
+            System.out.println(" Digite sua opção: ");
+            escolha = teclado.nextInt();
+        }while (escolha != 1);
+
+        Eventos meu_evento = new Eventos();
+        if (escolha == 1)
+        {
+            meu_evento.Inserir();
+        }
+        if (escolha == 2)
+        {
+            meu_evento.Excluir();
+        }
+    }
     void Inserir(){}
     void Excluir(){}
 
-    public Evento(String Nome, String Endereco, String Categoria, String Horario, String Descricao) {
-        this.Nome = nome;
-        this.Endereco = endereco;
-        this.Categoria = categoria;
-        this.Horario = horario;
-        this.Descricao = descricao;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public String getEndereco() {
-        return endereco;
-    }
-    public String getCategoria() {
-        return senha;
-    }
-    public String getHorario() {
-        return horario;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-
-    @Override
-    public String toString() {
-        return "Nome: " + nome + ", Endereco: " + endereco ", Senha: " + senha ", Horario: " + horario ", Descricao: " + descricao;
-    }
 }
